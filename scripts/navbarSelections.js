6e6043d11d2a3ce1129655a05319a439a7afb157
Justin To <74964260+JustinTo233@users.noreply.github.com>
2023-09-27 20:42:17 -0700

    8th commit
    added more images
    added images to collections.html

--- scripts/navbarSelections.js
+++ scripts/navbarSelections.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <div class="product-card">
         <div class="product-image">
             <a href="product_details.html?id=${product.id}" class="view-details">
-                <img src="${product.image}" alt="Image">
+                <img src="${product.image[0]}" alt="Image">
             </a>
         </div>
         <div class="product-details">
